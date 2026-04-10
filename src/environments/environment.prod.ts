e71d39cfc7176985af3b2a83614cf63d90f11e81
João Fernandes <joao.fernandes@pearlsofportugal.com>
2026-04-10 16:01:05 +0100

    fixed apiURL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://mvp-backend-41588214705.europe-west1.run.app',
+  apiUrl: 'https://mvp-backend-4270269089.europe-west1.run.app',
   apiKey: '123'
 
 };
